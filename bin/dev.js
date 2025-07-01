0b536cfc57ec6f3ef71663fd7ca29c35f53dbbc6
henrique <bongador@protonmail.com>
2025-06-30 18:26:23 -0600

    Test

--- bin/dev.js
+++ bin/dev.js
@@ -8,10 +8,23 @@ const app = express();
 
 const { SYNAPSE_PRIVATE_KEY, SYNAPSE_NETWORK,SYNAPSE_RPC_URL,PORT } = process.env;
 
-
+if(SYNAPSE_PRIVATE_KEY === undefined) {
+    console.log('`SYNAPSE_PRIVATE_KEY` not set. Copy .env.example to .env first.');
+    process.exit(1);
+}
+if(SYNAPSE_NETWORK === undefined) {
+    console.log('`SYNAPSE_NETWORK` not set. Copy .env.example to .env first.');
+    process.exit(1);
+}
+if(SYNAPSE_RPC_URL === undefined) {
+    console.log('`SYNAPSE_RPC_URL` not set. Copy .env.example to .env first.');
+    process.exit(1);
+}
 app.get('/', async (req, res) => {
     const { url } = req.query;
+    res.send(url);
 
+    /*
     const event = {
         body: JSON.stringify({
             args: {
@@ -28,6 +41,7 @@ app.get('/', async (req, res) => {
     const result = await handler(event)
 
     res.status(result.statusCode).send(result.body);
+    */
 });
 
 const port = PORT || 3000;
